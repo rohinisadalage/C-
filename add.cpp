eacae0b3e7edcca9e2a3cf66c16f90562b993261
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-15 08:14:42 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter the valude a:";
+	cin >>a;
+	cout<<"Enter the valude b:";
+	cin >>b;
+	sum=a+b;
+	cout<<a<<"+"<<b<<"="<<sum;
+	return 0;
+}
