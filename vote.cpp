39b3951fc57b154f2f06e320243c3ae39ffc12fd
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-18 17:56:51 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter the age:";
+	cin>>age;
+	if(age>=18)
+	{
+		cout<<"You can eligible for voting.......";
+	}
+	else
+	{
+		cout<<"you can't eligible for voting.......";
+	}
+	return 0;
+}
