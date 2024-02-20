1ed6f0e9344b014d2c123d18ed885780af89ec21
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-02-20 22:54:31 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char i;
+	cout<<"Enter the character:"<<endl;
+	cin>>i;
+	if(i=='a'|| i=='e'|| i=='i'|| i=='o'|| i=='u')
+	{
+		cout<<"You enter character is vowel..."<<endl;
+	}
+	else
+	{
+		cout<<"You enter character is consonent..."<<endl;
+	}
+  return 0;	
+}
