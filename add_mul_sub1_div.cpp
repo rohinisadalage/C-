ecd68f86afb2ef8bec90bafbeb1316462cadd276
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-03-01 08:20:33 +0530

    Add files via upload

---
+++ add_mul_sub1_div.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the value for a:"<<endl;
+	cin>>a;
+	cout<<"Enter the value for b:"<<endl;
+	cin>>b;
+	c=a+b;
+	cout<<c;
+	c=a-b;
+	cout<<c;
+	c=a*b;
+	cout<<c;
+	c=a/b;
+	cout<<c;
+	
+	return 0;
+}
+
