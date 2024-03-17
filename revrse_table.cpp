7d5214e94f329cf37d29be8d1bf8846113f0a791
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-03-17 20:37:28 +0530

    Add files via upload

---
+++ revrse_table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i;
+	cout<<"Enter the any number"<<endl;
+	cin>>n;
+	
+	for(i=10; i>=1; i--)
+	{
+		 cout<<""<<i*n<<endl;
+	}
+	cout<<"Thank You...";
+
+	return 0;
+}
