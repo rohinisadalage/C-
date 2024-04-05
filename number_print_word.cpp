34eaa1f84edbca4aafc86e728d268f734aad5ef9
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-05 01:08:44 -0700

    Add files via upload

---
+++ number_print_word.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter any number:";
+	cin>>n;
+	if(n==1)
+	{
+		cout<<"One";
+	}
+	else if(n==2)
+	{
+		cout<<"Two";
+	}
+	else if(n==3)
+	{
+		cout<<"Three";
+	}
+	else if(n==4)
+	{
+		cout<<"Four";
+	}
+	else if(n==5)
+	{
+		cout<<"Five";
+	}
+	else if(n==6)
+	{
+		cout<<"Six";
+	}
+	else if(n==7)
+	{
+		cout<<"Seven";
+	}
+	else if(n==8)
+	{
+		cout<<"Eight";
+	}
+	else if(n==9)
+	{
+		cout<<"Nine";
+	}
+	else
+	{
+        cout<<"Greater thean 9";
+	}
+	return 0;
+}
