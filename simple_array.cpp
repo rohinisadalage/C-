0762b820a8418458c487a1822d8c8e88c405d4d9
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-06 23:27:20 +0530

    Create simple_array.cpp

---
+++ simple_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // Declaring an array of integers with size 5
+    int arr[5];
+
+    // Initializing array elements
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    // Accessing and printing array elements
+    std::cout << "Array elements: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
