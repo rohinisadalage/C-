11a6dcaf0f5d7c8e21ee799ab8ceb9f7aef51a9d
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-07 19:17:21 +0530

    Add files via upload

---
+++ primitive_datatype_range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "size of int data type:\t" << sizeof (int) << endl;
+    cout << "size of long data type:\t" << sizeof (long) << endl;
+    cout << "size of char data type:\t" << sizeof (char) << endl;
+    cout << "size of bool data type:\t" << sizeof (bool) << endl;
+    cout << "size of float data type:\t" << sizeof (float) << endl;
+    cout << "size of double data type:\t" << sizeof (double) << endl;
+    cout << "size of short data type:\t" << sizeof (short) << endl;
+    return 0;
+}
