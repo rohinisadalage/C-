48c1d7280527a64838fb4bf746e7727f636eda33
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-14 23:46:07 +0530

    Create or_operator.cpp

---
+++ or_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 10;
+    int result;
+    result = (a || b);
+    cout << "Result: " << result <<endl;
+    return 0;
+}
