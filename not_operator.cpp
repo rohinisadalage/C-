ee612f5a582b2349efe04065ebfe5e3d6ecd35cf
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-15 19:51:46 +0530

    Create not_operator.cpp

---
+++ not_operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 10;
+    bool result;
+    result = !num;
+    cout << "Original value of num: " << num << endl;
+    cout << "Value of num after applying NOT operator: " << result <<endl;
+
+    return 0;
+}
