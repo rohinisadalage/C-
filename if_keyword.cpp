b9eb7e32af789fc083a6d3afb6a88dc6d18dcee5
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-28 19:31:30 +0530

    Add files via upload

---
+++ if_keyword.cpp
@@ -0,0 +1,21 @@
+//Write a cpp code to implement the if_else keyword
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"a is greatr than b:";
+    }
+    else
+    {
+        cout<<"b is greatr than a:";
+    }
+    cout<<"Thank You....";
+    return 0;
+}
