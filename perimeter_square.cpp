3cec8a2ba210f24cc5a060e364bb89fda5182ccd
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-04-30 18:26:52 +0530

    Create perimeter_square.cpp

---
+++ perimeter_square.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float side = 3, area;
+     area = 4 * side ;
+     cout << "Perimeter of Square having side " << side << " is :--> " << area;
+  cout<<endl<<"Thank You...";
+     return 0;
+}
