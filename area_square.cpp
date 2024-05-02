bcdf403d1dddde1b4f421550bbf35619dc05aa3d
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-05-02 11:32:33 +0530

    Add files via upload

---
+++ area_square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int area,l,b;
+	cout<<"Enter the value of and b respectively"<<endl;
+	cin>>l>>b;
+	area=l*b;
+	cout<<"Area of square is:"<<area;
+	return 0;
+}
