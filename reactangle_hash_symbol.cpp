10fbe6959a4f00df6eaa095ecd13d39e6e313a1e
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-06-03 21:58:54 +0530

    Create reactangle_hash_symbol.cpp

---
+++ reactangle_hash_symbol.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void printRectangle(int width, int height) {
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            std::cout << "#";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int width, height;
+
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    std::cout << "Enter the height of the rectangle: ";
+    std::cin >> height;
+
+    std::cout << std::endl;
+
+    printRectangle(width, height);
+
+    return 0;
+}
