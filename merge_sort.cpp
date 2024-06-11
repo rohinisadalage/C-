1294e8b7b717cff3610755ff3a0c13e78e24f4a6
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-06-11 20:25:50 +0530

    Create merge_sort.cpp

---
+++ merge_sort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+// Function to merge two subarrays of arr[]
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temp arrays
+    int* leftArray = new int[n1];
+    int* rightArray = new int[n2];
+
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (int i = 0; i < n1; i++)
+        leftArray[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArray[j] = arr[mid + 1 + j];
+
+    // Merge the temp arrays back into arr[left..right]
+    int i = 0; // Initial index of first subarray
+    int j = 0; // Initial index of second subarray
+    int k = left; // Initial index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of leftArray[], if there are any
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of rightArray[], if there are any
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+
+    // Free the memory of temp arrays
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+// Function to implement merge sort
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        // Find the middle point
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Main function to test the merge sort algorithm
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arrSize);
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arrSize);
+    return 0;
+}
