0a7540e9ca850c26eb6c9fd44751defa3ea11b2c
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-07-03 18:02:04 +0530

    Create left_tringle_and_symbol.cpp

---
+++ left_tringle_and_symbol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int rows;
+
+    // Number of rows for the triangle
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            std::cout << "& ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
