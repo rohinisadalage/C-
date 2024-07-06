56e42cb138fdea6130137ae85b1056ae372f95b2
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-07-06 09:49:19 +0530

    Create diamond_and_symbol.cpp

---
+++ diamond_and_symbol.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j, space;
+
+    // Enter the number of rows for the diamond (half of the diamond)
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // Upper half of the diamond
+    for (i = 1; i <= n; i++) {
+        for (space = 1; space <= n - i; space++) {
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            cout << "&";
+        }
+        cout << endl;
+    }
+
+    // Lower half of the diamond
+    for (i = n - 1; i >= 1; i--) {
+        for (space = 1; space <= n - i; space++) {
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            cout << "&";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
