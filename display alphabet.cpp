12a7c4d880032542f7014e971ffa8028f10eba77
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-07-24 08:48:44 +0530

    Create display alphabet.cpp

---
+++ display alphabet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Alphabets from A to Z are:" << endl;
+
+    for(ch = 'A'; ch <= 'Z'; ch++) {
+        cout << ch << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
