8191156ec0480d82aceb3fcbb4837c74d756513e
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-07-31 20:12:42 +0530

    Create square root.cpp

---
+++ square root.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n,a;
+  cout<<"Enter the number you want to get the square root of those number: "<<endl;
+  cin>>n;
+  a=n*n;
+  cout<<"the square root of the number is: "<<a;
+    return 0;
+}
