3c7c77ddbe1cb1b653d980cbed677157b1dfd0cd
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-08-06 22:12:39 +0530

    Create Matrix.cpp

---
+++ Matrix.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    int matrix[rows][cols];
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << "Element [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "The matrix is:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
