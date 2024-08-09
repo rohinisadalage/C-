3e832a43dd5cbfa031ef1a9f61dbcfb63fba3560
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-08-09 22:07:10 +0530

    Create Square.cpp

---
+++ Square.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    int number;
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+    std::cout << "The square of " << number << " is " << number * number << std::endl;
+    return 0;
+}
