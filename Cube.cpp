51cf180dbaeffc1f91cb31853eb6ad6c3a16e97e
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-08-10 22:53:39 +0530

    Create Cube.cpp

---
+++ Cube.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num, cube;
+    cout << "Enter a number: ";
+    cin >> num;
+    cube = num * num * nun;
+    cout << "The cube of " << num << " is " << cube << endl;
+    return 0;
+}
