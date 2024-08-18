792297ea4475fbe078fb74aa3e7e4a7b7f48a283
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2024-08-18 22:50:27 +0530

    Create Multiplication in CPP.cpp

---
+++ Multiplication in CPP.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    int num1, num2, product;
+    std::cout << "Enter two integers: ";
+    std::cin >> num1 >> num2;
+    product = num1 * num2;
+    std::cout << "Product of " << num1 << " and " << num2 << " is: " << product << std::endl;
+    return 0;
+}
