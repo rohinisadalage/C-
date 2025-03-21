5bbe68de25e9145a7dcd2666a70699ec51325a99
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-03-21 08:24:26 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,20 @@
+//W.a.cpp.code to implement the area of the circle
+#include <iostream>
+using namespace std;
+int main()
+{
+   float r,area;
+    cout<<"Enter the radius of the circle: ";
+    cin>>r;
+  
+    cout<<endl;
+    area= 3.14*r*r;
+    cout<<"Area of the Circle is: "<< area <<endl;
+    cout<<"Thank You...!";
+    return 0;
+}
+
+
+
+
+
