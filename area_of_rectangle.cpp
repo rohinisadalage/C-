749716be980c4d58578fc9a89339db82c1253086
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-03-22 20:11:08 +0530

    Add files via upload

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,21 @@
+//W.a.cpp.code to implement the area of the rectangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int area, length, breadth;
+    cout<<"Enter the length of the rectangle: ";
+    cin>>length;
+    cout<<"Enter the breadth of the rectangle: ";
+    cin>>breadth;
+    cout<<endl;
+    area=length*breadth;
+    cout<<"Area of the rectangle is: "<< area <<endl;
+    cout<<"Thank You...!";
+    return 0;
+}
+
+
+
+
+
