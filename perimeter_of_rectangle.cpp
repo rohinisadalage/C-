9bab16ad45f1f71c406092bd23d062669c049ca2
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-03-28 10:01:57 +0530

    Add files via upload

---
+++ perimeter_of_rectangle.cpp
@@ -0,0 +1,18 @@
+//W.a.cpp.code to implement the perimeter of the rectangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, length, width;
+    cout<<"Enter the length of the rectangle: ";
+    cin>>length;
+    cout<<"Enter the width of the rectangle: ";
+    cin>>width;
+
+    a= 2*(length + width);
+    cout<<"Perimter of the rectangle is: "<<a <<endl;
+    cout<<"Thank You...!";
+    return 0;
+}
+
+
