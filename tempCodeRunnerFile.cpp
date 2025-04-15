e29ea817c6c36a63b9b8f9211298571ecd9598d9
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-04-15 20:24:14 +0530

    Add files via upload

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,17 @@
- // cout<<"Multiplication of those number is: "<<c<<endl;
-    // cout<<"Division of those number is: "<<c<<endl;
+#include <iostream>
+// using namespace std;
+// int mian()
+// {
+//     int n,x=0, y=1, z=0;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     while(z<=n)
+//     {
+//         cout<<endl<<z;
+//         x=y;
+//         y=z;
+//         z=x+y;
+//     }
+//     cout<<endl<<"Thank You";
+//     return 0;
+// }
