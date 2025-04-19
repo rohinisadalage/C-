b9db4db3f71f3dfe2a37ce70e287c2cd122116f2
rohinisadalage <159412218+rohinisadalage@users.noreply.github.com>
2025-04-19 19:00:36 +0530

    Add files via upload

---
+++ 1_to_nwhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"Enter the Value of n: ";
+    cin>>n;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i=i+1;
+    }
+    cout<<"Thank You...!";
+    return 0;
+}
